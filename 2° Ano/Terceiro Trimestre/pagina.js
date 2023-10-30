e49ebe85fb57848944f711b636337f4a56580ab4
Emily Serafim <130461632+EmilySeraf@users.noreply.github.com>
2023-10-30 09:18:09 -0300

    Add files via upload

---
+++ 2° Ano/Terceiro Trimestre/pagina.js
@@ -0,0 +1,6 @@
+function mudarCor () {
+    const fundo = document.querySelector("body")
+    const texto = document.querySelector("h1")
+    fundo.style.backgroundColor = 'slateblue'
+    texto.style.color = 'white'
+}
