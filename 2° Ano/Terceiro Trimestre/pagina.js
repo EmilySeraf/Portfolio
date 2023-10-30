92f55e77b9b89826d22af37984cf57ee2cd71c4c
Emily Serafim <130461632+EmilySeraf@users.noreply.github.com>
2023-10-30 09:19:30 -0300

    Delete 2° Ano/Terceiro Trimestre/pagina.js

--- 2° Ano/Terceiro Trimestre/pagina.js
+++
@@ -1,6 +0,0 @@
-function mudarCor () {
-    const fundo = document.querySelector("body")
-    const texto = document.querySelector("h1")
-    fundo.style.backgroundColor = 'slateblue'
-    texto.style.color = 'white'
-}
